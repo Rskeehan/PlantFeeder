cf2a0676d616f55fe4dfbc62e3d2b1fdd9e02e74
Rob <rskeehan@gmail.com>
2024-05-19 17:37:24 -0500

    chenged the name of the container being built/run

--- App/build+run.ps1
+++ App/build+run.ps1
@@ -1,3 +1,3 @@
 cd "C:\Users\RPS00\OneDrive\Documents\03 Coding\PlantFeeder\App"
-docker build -t app .
-docker run -d --name app --env-file .env app
+docker build -t plant-feeder .
+docker run -d --env-file .env plant-feeder
